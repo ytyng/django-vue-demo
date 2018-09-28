50849c49aeb60e6d556b053d478000e88c4cc8eb
MasatoraSakikoyama <sakikoyama@torico-tokyo.com>
2018-09-28 15:39:00 +0900

    修正

---
+++ demo/statics/src/components/items/index.js
@@ -0,0 +1,3 @@
+export default {
+// wraite componet here
+}
--- demo/statics/src/index.js
+++ demo/statics/src/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import template from './template.html';
 import './style.scss';
+// import component here
 
 const app = new Vue({
   el: '#demo-items',
